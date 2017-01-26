e5bf84f0d1010373ae78086d6584bd4b9173b926
piotrek0019 <piotrek019@wp.pl>
2017-01-26 14:08:25 +0000

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace ProfHenryCopy
+{
+    class Program
+    {
+        static public void CopyDirectHenr1(string sourceDirectory, string destDirectory)
+        {
+            
+
+            if (!Directory.Exists(destDirectory))
+                Directory.CreateDirectory(destDirectory);
+
+            string[] files = Directory.GetFiles(sourceDirectory);
+            foreach (string file in files)
+            {
+                string name = Path.GetFileName(file);
+                string dest = Path.Combine(destDirectory, name);
+                File.Copy(file, dest);
+            }
+            string[] folders = Directory.GetDirectories(sourceDirectory);
+
+            Console.WriteLine("Trwa kopiowanie folder 1");
+
+            foreach (string folder in folders)
+            {
+                string name = Path.GetFileName(folder);
+                string dest = Path.Combine(destDirectory, name);
+                CopyDirectHenr1(folder, dest);
+            }
+        }
+
+        static public void CopyDirectHenr2(string sourceDirectory, string destDirectory)
+    {
+         
+
+            if (!Directory.Exists(destDirectory))
+                Directory.CreateDirectory(destDirectory);
+
+            string[] files = Directory.GetFiles(sourceDirectory);
+            foreach (string file in files)
+            {
+                string name = Path.GetFileName(file);
+                string dest = Path.Combine(destDirectory, name);
+                File.Copy(file, dest);
+            }
+            string[] folders = Directory.GetDirectories(sourceDirectory);
+
+            Console.WriteLine("Copy directory 2");
+
+            foreach (string folder in folders)
+            {
+                string name = Path.GetFileName(folder);
+                string dest = Path.Combine(destDirectory, name);
+                CopyDirectHenr2(folder, dest);
+            }
+
+    }
+    
+        static public void CopyFileHenry(string fileSource, string destFile)
+        {
+            string file = fileSource;
+            Console.WriteLine("Copy file ...");
+            string name = Path.GetFileName(file);
+            string dest = Path.Combine(destFile, name);
+            File.Copy(file, dest);
+        }
+ 
+
+        static public void CopyFolder2(string sourceDirecotry, string destDirecotry, bool overwrite)
+        {
+         
+
+            Directory.CreateDirectory(destDirecotry);
+
+            string[] files = Directory.GetFiles(sourceDirecotry);
+            foreach (string file in files)
+            {
+                string name = Path.GetFileName(file);
+                string dest = Path.Combine(destDirecotry, name);
+                File.Copy(file, dest, true);
+            }
+            string[] folders = Directory.GetDirectories(sourceDirecotry);
+            
+            Console.WriteLine("Copy directory 3");
+
+            foreach (string folder in folders)
+            {
+                string name = Path.GetFileName(folder);
+                string dest = Path.Combine(destDirecotry, name);
+                CopyFolder2(folder, dest, true);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmss"));
+            string sourceDirectory = @"D:\Programy\path1\1";
+            string sourceDirectory3 = @"D:\Programy\path2\db";
+            string file = @"D:\Programy\path3\usr_v2.t";
+            string destDirecotry = @"E:\OneDrive\path4\postępy_w_nauce_";
+           
+            string sourceDirectory2 = @"E:path5";
+            string destDirecotry2 = @"E:\OneDrive\path6\Piotr_dok";
+
+            string dataTime = DateTime.Now.ToString("yyyyMMddHHmmss");
+            destDirecotry += dataTime;
+            try
+            {
+                Console.ReadLine();
+
+                if (!Directory.Exists(destDirecotry))
+                {
+                    if (Directory.Exists(destDirecotry))
+                    {
+                        destDirecotry += 1;
+                    }
+                    Directory.CreateDirectory(destDirecotry);
+                }
+
+
+
+                CopyDirectHenr1(sourceDirectory, destDirecotry + @"\1");
+                Console.WriteLine("Copy directory 1 finished");
+
+                CopyDirectHenr2(sourceDirectory3, destDirecotry + @"\db");
+                Console.WriteLine("Copy directory db finished");
+
+                CopyFileHenry(file, destDirecotry);
+                Console.WriteLine("Copy file usr_v2 finished");
+            
+                CopyFolder2(sourceDirectory2, destDirecotry2, true);
+                Console.WriteLine("Copy directory 3 finished");
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine("Error: " + Ex.Message);
+            }
+
+            Console.WriteLine("Press Enter");
+            Console.ReadLine();
+        }
+    }
+}
